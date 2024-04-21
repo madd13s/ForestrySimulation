ff5635d29995ff9e62e03d2640b6bc0a111a676e
maddie <madelyn.s.377@gmail.com>
2024-04-21 14:57:20 -0400

    First commit

---
+++ src/Forest.java
@@ -0,0 +1,264 @@
+import java.io.*;
+import java.util.ArrayList;
+
+/**
+ * Represents a forest that contains multiple Trees.
+ * @author Madelyn Steinour
+ * @version 1.0
+ * @see Forest
+ */
+public class Forest {
+
+    // Variables
+    private String name; // The name of the forest
+    private ArrayList<Tree> trees; // The list of trees in the forest
+
+    /**
+     * Constructs a forest given a certain name.
+     *
+     * @param name The name of the forest.
+     * @see Forest
+     */
+    public Forest(String name) {
+        this.name = name;
+        this.trees = new ArrayList<>();
+
+    }// End of Forest constructor
+
+    /**
+     * Loads data from a CSV file and populates the forest.
+     *
+     * @param csvFilePath The path to the CSV file with Tree data.
+     * @return true If loading is successful.
+     * @return false If loading is unsuccessful.
+     * @throws IOException If an I/O occurs while reading the file.
+     * @throws NumberFormatException If the format of the data in the file is incorrect.
+     * @see Forest
+     */
+    public boolean loadForest(String csvFilePath) {
+
+        // Try-catch block that tries to read the CSV file
+        try (BufferedReader reader = new BufferedReader(new FileReader(csvFilePath))) {
+            String line;
+
+            // Reads the tree data separated by commas from the CSV file
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+                Tree.Species species = convertToSpeciesEnum(data[0]);
+
+                // Makes sure the tree species is valid
+                if (species == null) {
+                    System.out.println("Invalid species in the CSV file.");
+                    return false;
+                }
+
+                // Converts the CSV data to Tree data
+                int yearPlanted = Integer.parseInt(data[1]);
+                double height = Double.parseDouble(data[2]);
+                double growthRate = Double.parseDouble(data[3]);
+
+                Tree tree = new Tree(species, yearPlanted, height, growthRate);
+                trees.add(tree);
+            }
+
+            return true;
+
+        } catch (IOException | NumberFormatException e) {
+            return false;
+
+        }// End of try-catch block
+
+    }// End of loadForest method
+
+    /**
+     * Loads tree data from a database file and populates the forest.
+     *
+     * @param dbFileName The name of the database file.
+     * @return true If loading is successful.
+     * @return false If loading is unsuccessful.
+     * @throws IOException If an I/O error occurs while reading the file.
+     * @throws IllegalArgumentException If the data in the file is invalid or corrupted.
+     * @see Forest
+     */
+    public boolean loadForestFromDB(String dbFileName) {
+
+        // Try-catch block that tries to read the name of a database file
+            try (BufferedReader reader = new BufferedReader(new FileReader(dbFileName))) {
+                String line;
+
+                // Reads the tree data separated by commas
+                while ((line = reader.readLine()) != null) {
+
+                    String[] data = line.split(",");
+
+                    // Converts database data to Tree data
+                    Tree.Species species = Tree.Species.valueOf(data[0]);
+                    int yearPlanted = Integer.parseInt(data[1]);
+                    double height = Double.parseDouble(data[2]);
+                    double growthRate = Double.parseDouble(data[3]);
+
+                    Tree tree = new Tree(species, yearPlanted, height, growthRate);
+                    this.trees.add(tree);
+                }
+                return true;
+
+            } catch (IOException | IllegalArgumentException exception) {
+            return false;
+
+            }// End of try-catch block
+
+        }// End of loadForestFromDB method
+
+    /**
+     * Converts a string of a Tree species to its enum value.
+     *
+     * @param speciesString The string representation of the species.
+     * @return species The enum value of the species string.
+     * @return null If the species string has no corresponding enum.
+     * @see Forest
+     */
+    private Tree.Species convertToSpeciesEnum(String speciesString) {
+
+        // Converts Strings to Enums
+        for (Tree.Species species : Tree.Species.values()) {
+
+            if (species.toString().equalsIgnoreCase(speciesString)) {
+                return species;
+            }
+        }
+        return null; // If the species string does not match any enum constant
+
+    }// End of convertToSpeciesEnum method
+
+    /**
+     * Prints the forest name, its Trees, and the average height.
+     *
+     * @see Forest
+     */
+    public void printForest() {
+        System.out.println("Forest name: " + name);
+
+        int index;
+        for (index = 0; index < trees.size(); index++) {
+            System.out.println("     " + index + " " + trees.get(index));
+        }
+        System.out.println("There are " + trees.size() + " trees, with an average height of " + calculateAverageHeight());
+
+    }// End of printForest method
+
+    /**
+     * Adds a randomly generated Tree to the forest.
+     *
+     * @see Forest
+     */
+    public void addRandomTree() {
+
+        Tree tree = Tree.generateRandomTree();
+        trees.add(tree);
+
+    }// End of addRandomTree method
+
+    /**
+     * Removes a Tree from the forest based on an index.
+     *
+     * @param index The index of the tree to be removed.
+     * @see Forest
+     */
+    public void cutDownTree(int index) {
+
+        // Make sure tree number is valid
+        if (index >= 0 && index < trees.size()) {
+            trees.remove(index);
+
+        } else {
+            System.out.println("Tree number " + index + " does not exist");
+        }
+
+    }// End of cutDownTree method
+
+    /**
+     * Simulates a year of growth for all trees in the forest.
+     *
+     * @see Forest
+     */
+    public void simulateYear() {
+
+        for (Tree tree : trees) {
+            tree.simulateYear();
+        }
+
+
+    }// End of simulateYear method
+
+    /**
+     * Reaps and replaces trees from the forest that are taller than a specified height.
+     *
+     * @param height The minimum height to be reaped.
+     * @see Forest
+     */
+    public void reapForest(double height) {
+
+        int index;
+        for (index = 0; index < trees.size(); index++) {
+
+            // Make sure the reaped trees are tall enough
+            if (trees.get(index).getHeight() > height) {
+
+                System.out.println("Reaping the tall tree " + trees.get(index));
+
+                // Replace the reaped tree with a new tree
+                Tree newTree = Tree.generateRandomTree();
+                trees.set(index, newTree);
+                System.out.println("Replaced with new tree " + newTree);
+            }
+
+        }// End of for loop
+
+    }// End of reapForest method
+
+    /**
+     * Saves the forest data to a database file.
+     *
+     * @throws FileNotFoundException If the file cannot be created.
+     * @see Forest
+     */
+    public void saveForest() {
+
+        // Try-catch block that tries to save the forest as a database file
+        try (PrintWriter writer = new PrintWriter(name + ".db")) {
+
+            for (Tree tree : trees) {
+                writer.println(tree.toCSV());
+            }
+            System.out.println("Forest saved to " + name + ".db");
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+
+        }// End of try catch block
+
+    }// End of saveForest method
+
+    /**
+     * Calculates the average height of all trees in the forest.
+     *
+     * @return averageHeight Average height of all trees rounded to two decimal places.
+     * @see Forest
+     */
+    private double calculateAverageHeight() {
+
+        // Get total height
+        double totalHeight = 0;
+        for (Tree tree : trees) {
+            totalHeight += tree.getHeight();
+        }
+
+        // Divide total height by amount of trees
+        double averageHeight = trees.isEmpty() ? 0 : totalHeight / trees.size();
+
+        return Math.round(averageHeight * 100.0) / 100.0; // rounding to two decimal places
+
+    }// End of calculateAverageHeight method
+
+}// End of Forest class
+
---
+++ src/Tree.java
@@ -0,0 +1,106 @@
+import java.util.Random;
+
+/**
+ * Represents a tree with species, year planted, height, and growth rate.
+ * @author Madelyn Steinour
+ * @version 1.0
+ * @see Tree
+ */
+public class Tree {
+
+    // Constants
+    private static final int MINIMUM_YEAR_PLANTED = 2000;
+    private static final int MAXIMUM_YEAR_PLANTED = 25;
+    private static final double MINIMUM_HEIGHT = 10.0;
+    private static final double MAXIMUM_HEIGHT = 20.0;
+    private static final double MINIMUM_GROWTH_RATE = 10.0;
+    private static final double MAXIMUM_GROWTH_RATE = 20.0;
+
+    // Variables
+    public enum Species { Birch, Maple, Fir }
+    private Species species; // The species of the tree
+    private int yearPlanted; // The year the tree was planted
+    private double height; // The height of the tree in feet
+    private double growthRate; // The growth rate as a percentage
+
+    /**
+     * Constructor to initialize Tree variables.
+     *
+     * @param species Species of the Tree.
+     * @param yearPlanted Year the Tree was planted.
+     * @param height Height of the Tree in feet.
+     * @param growthRate Growth rate as a percentage.
+     * @see Tree
+     */
+    public Tree(Species species, int yearPlanted, double height, double growthRate) {
+        this.species = species;
+        this.yearPlanted = yearPlanted;
+        this.height = height;
+        this.growthRate = growthRate;
+    }// End of Tree constructor
+
+    /**
+     * Generates a random tree.
+     *
+     * @return Tree A tree with random species, year planted, height, and growth rate.
+     * @see Tree
+     */
+    public static Tree generateRandomTree() {
+        Random random = new Random();
+        Species[] speciesArray = Species.values();
+        Species species = speciesArray[random.nextInt(speciesArray.length)];
+
+        int yearPlanted = MINIMUM_YEAR_PLANTED + random.nextInt(MAXIMUM_YEAR_PLANTED); // Year between 2000 and 2024
+        double height = MINIMUM_HEIGHT + random.nextDouble() * MAXIMUM_HEIGHT; // Height between 10 and 20 feet
+        double growthRate = MINIMUM_GROWTH_RATE + random.nextDouble() * MAXIMUM_GROWTH_RATE; // Growth rate between 10% and 20%
+        return new Tree(species, yearPlanted, height, growthRate);
+
+    }// End of generateRandomTree method
+
+    /**
+     * Simulates a year of growth based on a Tree's growth rate.
+     *
+     * @see Tree
+     */
+    public void simulateYear() {
+        height *= 1 + growthRate / 100;
+
+    }// End of simulateYear method
+
+    /**
+     * Retrieve the height of a tree.
+     *
+     * @return height The Tree height in feet.
+     * @see Tree
+     */
+    public double getHeight() {
+        return height;
+
+    }// End of getHeight method
+
+    /**
+     * Converts the Tree variables to CSV format.
+     *
+     * @return string Tree species, year planted, height, and growth rate in CSV format.
+     * @see Tree
+     */
+    public String toCSV() {
+        return species + "," + yearPlanted + "," + height + "," + growthRate;
+
+    }// End of toCSV method
+
+    /**
+     * Constructs a string of the Tree information.
+     *
+     * @return string Tree species, year planted, height, and growth rate.
+     * @see Tree
+     */
+    @Override
+    public String toString() {
+        return species + " " + yearPlanted + " " + String.format("%.2f", height) + "' " + String.format("%.1f", growthRate) + "%";
+
+    }// End of toString method
+
+}// End of Tree class
+
+
